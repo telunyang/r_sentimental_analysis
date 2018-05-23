4701e12c495500e2d4d66304b4fe4c96939ac606
Darren Yang <telunyang@gmail.com>
2018-05-24 06:04:12 +0800

    update files

--- exec.php
+++ exec.php
@@ -1,6 +1,6 @@
 <?php
 if( isset($_POST['url']) ){
     $file_name = shell_exec("Rscript sa.R ".$_POST['url']);
-    echo "http://localhost/R-Sentimental-Analysis/".$file_name;
+    echo "http://210.242.90.110/R/".$file_name;
 }
 ?>
